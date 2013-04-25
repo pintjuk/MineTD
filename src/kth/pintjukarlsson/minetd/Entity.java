350b5a97dd95f7b32803707f99d2c2af67deac37
mk4 <mk4@lavabit.com>
2013-04-25 15:07:36 +0200

    added some classes

---
+++ src/kth/pintjukarlsson/minetd/Entity.java
@@ -0,0 +1,13 @@
+package kth.pintjukarlsson.minetd;
+
+import com.badlogic.gdx.math.Vector2;
+	
+public class Entity {
+	
+	public Vector2 pos;
+	
+	public Entity(int x, int y) {
+		pos = new Vector2(x, y);
+	}
+	
+}
