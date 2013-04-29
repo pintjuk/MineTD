76212cb0d046b275f8d3e15cef3aa01ce1f7ddb2
mk4 <mk4@lavabit.com>
2013-04-29 15:39:49 +0200

    updated EnemyManager class

--- src/kth/pintjukarlsson/minetd/BuildingManager.java
+++ src/kth/pintjukarlsson/minetd/BuildingManager.java
@@ -2,11 +2,24 @@ package kth.pintjukarlsson.minetd;
 
 public class BuildingManager {
 	
-	private Building[] buildings;
 	private final static int MAX_BUILDINGS = 256;
 	
+	private Building[] buildings;
+	
 	public BuildingManager() {
-		buildings = new Building[2];
+		buildings = new Building[MAX_BUILDINGS];
 	}
 
+	// Runs the Update method for each existing Building
+	public void Update() {
+		for (Building b : buildings) {
+			b.Update();
+		}
+	}
+	// Runs the Draw method for each existing Building
+	public void Draw() {
+		for (Building b : buildings) {
+			b.Draw();
+		}
+	}
 }
--- src/kth/pintjukarlsson/minetd/EnemyManager.java
+++ src/kth/pintjukarlsson/minetd/EnemyManager.java
@@ -4,22 +4,40 @@ import com.badlogic.gdx.math.Vector2;
 
 public class EnemyManager {
 
-	private Enemy[] enemies;
 	private final static int MAX_ENEMIES = 25;
-	private final static int[] spawnPos = {0, 0};
+	private int[] spawnPos;
 	
-	// Creates a new enemy manager that can hold
-	// the default number of enemies
-	public EnemyManager() {
+	private Enemy[] enemies;
+	
+	private int numEnemies;
+	private int enemyHP;
+	private int enemySpeed;
+	
+	/**
+	 *  Creates a new enemy manager that spawns stuff at the location x, y.
+	 *  It starts out with some default values, and uses the maximum number of enemies.
+	 */
+	public EnemyManager(int x, int y) {
+		spawnPos = new int[] {x, y};
+		
 		enemies = new Enemy[MAX_ENEMIES];
+		numEnemies = MAX_ENEMIES;
+		enemyHP = 10;
+		enemySpeed = 50;
 	}
 	
 	// Spawns a new wave of enemies in a square at the default spawn location
 	private void spawnWave() {
-		int side = (int)Math.ceil(Math.sqrt(MAX_ENEMIES)); // side of square
-		for (int i=0; i<MAX_ENEMIES; i++) {
-			enemies[i] = new Enemy(spawnPos[0]+(i%side), spawnPos[1]+(i/side));
+		int side = (int)Math.ceil(Math.sqrt(numEnemies)); // side of square
+		for (int i=0; i<numEnemies; i++) {
+			int x = spawnPos[0]+(i%side);
+			int y = spawnPos[1]+(i/side);
+			enemies[i] = new Enemy(x, y, enemyHP, enemySpeed);
 		}
+		// Increase difficulty parameters for next wave
+		enemyHP *= 1.5;
+		enemySpeed += 1;
+		
 	}
 	
 	// Runs the Update() method for each existing enemy
